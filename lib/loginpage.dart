2a359131a2c2c2ad62e01765419eeba301b71d6d
Dilshad-Akthar <aktharvkdilshad@gmail.com>
2023-03-30 22:00:28 +0530

    login_page_1.0

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -8,6 +8,75 @@ import 'package:oru_app/homepage.dart';
 import 'package:oru_app/reusables.dart';
 
 class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:Color.fromARGB(255, 18, 88, 185),
+      appBar: AppBar(
+        title: const Text("Sreelu's", style: TextStyle(color: Color.fromARGB(255, 255, 253, 253))),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.fromLTRB(10, 200, 10, 0),
+        child: Column(
+          children: [
+            Container(
+              width: 70,
+              height: 70,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage('images/oxygen_cylinder_fill.png'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+              
+            ),
+            reusableTextField(
+              "Email",
+              Icons.person,
+              false,
+              email,
+              
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            reusableTextField(
+              "Password",
+              Icons.lock_outline,
+              true,
+              password,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            signInSignUpButton("Login", context, () {
+              postRequest();
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+
+/*
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
@@ -52,7 +121,7 @@ class _LoginPageState extends State<LoginPage> {
             ],
           ),
         ));
-  }
+  }*/
 
   Future<void> postRequest() async {
     final url = Uri.parse('http://soc-erp.showcase.code7.in/api/authenticate');
