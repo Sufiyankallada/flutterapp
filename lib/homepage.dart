3a64dd77d8b52ea7e99b363ce3b2b13c2d1fe917
Dilshad-Akthar <aktharvkdilshad@gmail.com>
2023-03-31 23:23:06 +0530

    app_exit_pop_up_added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -20,95 +20,125 @@ class _HomePageState extends State<HomePage> {
     //change item hieght to change the hieght of grid
     final double itemHeight = (size.height - kToolbarHeight - 24) / 3.2;
     final double itemWidth = size.width / 2;
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 170, 229, 238),
-      appBar: AppBar(
-        elevation: 0.9,
-        backgroundColor: Color.fromARGB(255, 73, 183, 202),
-        title: const Text('Day Scholars',
-            style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.w600,
-                fontSize: 18)),
-        actions: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              Text('Data Entry',
-                  style: TextStyle(
-                      color: Colors.black87,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 14)),
-            ],
-          ),
-          IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: const Icon(
-                Icons.power_settings_new,
-                color: Colors.redAccent,
-              )),
-        ],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 16),
-              child: GridView.count(
-                crossAxisCount: 2,
-                childAspectRatio: (itemWidth / itemHeight),
-                mainAxisSpacing: 8,
-                crossAxisSpacing: 8,
-                children: [
-                  GridButton(
-                      color: Colors.green[100]!,
-                      icon: Icons.add_circle_outline,
-                      label: "Fill Cylinders",
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => FillCylinders(
-                                      qrList: [],
-                                      accessToken: widget.access_token,
-                                    )));
-                      }),
-                  GridButton(
-                      color: Colors.blue[100]!,
-                      icon: Icons.delivery_dining,
-                      label: "Deliver Cylinders",
-                      onPressed: () {}),
-                  GridButton(
-                      color: Colors.pink[100]!,
-                      icon: Icons.directions_bike,
-                      label: "Pickup",
-                      onPressed: () {}),
-                  GridButton(
-                      color: Colors.orange[100]!,
-                      icon: Icons.local_shipping,
-                      label: "Unloading",
-                      onPressed: () {}),
-                  GridButton(
-                      color: Colors.purple[100]!,
-                      icon: Icons.person,
-                      label: "Manual",
-                      onPressed: () {}),
-                  GridButton(
-                      color: Colors.teal[100]!,
-                      icon: Icons.add,
-                      label: "Add New Cylinder",
-                      onPressed: () {}),
-                ],
+    return WillPopScope(
+      onWillPop: () => _onBackButtonPressed(context),
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 170, 229, 238),
+        appBar: AppBar(
+          elevation: 0.9,
+          backgroundColor: Color.fromARGB(255, 73, 183, 202),
+          title: const Text('Day Scholars',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18)),
+          actions: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                Text('Data Entry',
+                    style: TextStyle(
+                        color: Colors.black87,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14)),
+              ],
+            ),
+            IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.power_settings_new,
+                  color: Colors.redAccent,
+                )),
+          ],
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  childAspectRatio: (itemWidth / itemHeight),
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                  children: [
+                    GridButton(
+                        color: Colors.green[100]!,
+                        icon: Icons.add_circle_outline,
+                        label: "Fill Cylinders",
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => FillCylinders(
+                                        qrList: [],
+                                        accessToken: widget.access_token,
+                                      )));
+                        }),
+                    GridButton(
+                        color: Colors.blue[100]!,
+                        icon: Icons.delivery_dining,
+                        label: "Deliver Cylinders",
+                        onPressed: () {}),
+                    GridButton(
+                        color: Colors.pink[100]!,
+                        icon: Icons.directions_bike,
+                        label: "Pickup",
+                        onPressed: () {}),
+                    GridButton(
+                        color: Colors.orange[100]!,
+                        icon: Icons.local_shipping,
+                        label: "Unloading",
+                        onPressed: () {}),
+                    GridButton(
+                        color: Colors.purple[100]!,
+                        icon: Icons.person,
+                        label: "Manual",
+                        onPressed: () {}),
+                    GridButton(
+                        color: Colors.teal[100]!,
+                        icon: Icons.add,
+                        label: "Add New Cylinder",
+                        onPressed: () {}),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
+        drawer: buildDrawer(context),
       ),
-      drawer: buildDrawer(context),
     );
+  }
+
+  Future<bool> _onBackButtonPressed(context) async {
+    bool exitApp = await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Really"),
+            content: const Text("Are you sure ?"),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text("No"),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text("Yes"),
+              ),
+            ],
+          );
+        });
+    return exitApp != null ? exitApp : false;
+    //return exitApp ?? false;
   }
 }
 
